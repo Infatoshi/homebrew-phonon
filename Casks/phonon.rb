b357e2b1b90c74113862bca075ce8a644b8f52a7
Elliot Arledge <59771030+Infatoshi@users.noreply.github.com>
2026-07-22 13:39:26 -0600

    Declare Phonon cask source (#2)

--- Casks/phonon.rb
+++ Casks/phonon.rb
@@ -2,7 +2,8 @@ cask "phonon" do
   version "0.1.3"
   sha256 "2a8c53e4eed7b1c93ae865aa88ec72c126a0485fd02a791750e4f078e5fc7f7d"
 
-  url "https://github.com/Infatoshi/phonon/releases/download/v#{version}/Phonon.dmg"
+  url "https://github.com/Infatoshi/phonon/releases/download/v#{version}/Phonon.dmg",
+      verified: "github.com/Infatoshi/phonon/"
   name "Phonon"
   desc "Fast, local, open-source voice typing"
   homepage "https://phonon.sh/"
